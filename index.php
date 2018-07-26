33341a7b4e47f01b4b9152cbfc017a2921f6e27b
Md. Rajuanul Haque <rajuanulkuet@gmail.com>
2018-07-26 10:43:55 +0600

    Add files via upload

---
+++ index.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Khela.com</title>
+	<link rel="stylesheet" type="text/css" href="css/main.css">
+</head>
+<body>
+	<nav>
+		<div id="menu">
+			<ul id="menu-item">
+				<li>Home</li>
+				<li>Football</li>
+				<li>Cricket</li>
+				<li>Badminton</li>
+				<li>Busketball</li>
+				<li>Golf</li>
+				<li>Others</li>
+			</ul>
+		</div>
+	</nav>
+
+	<div>
+		<h2><center>Welcome to khela.com</center></h2>
+
+		<p id="info">
+			Here you will get information about your favourite sport.
+		</p>
+	</div>
+</body>
+</html>
